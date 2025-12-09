9db3cdeab861ad0b30990e4a76f939aeb19a53f3
Bibek Bhattarai <bb.bibekbhattarai@gmail.com>
2025-12-09 23:55:21 +0545

    reverse number

---
+++ big-o.py
@@ -0,0 +1,13 @@
+def get_first(arr):
+    return arr[0]
+
+def print_all(arr):
+    for x in arr:
+        print(x)
+
+def print_pairs(arr):
+    for i in range(len(arr)):
+        for j in range(len(arr)):
+            print(arr[i], arr[j])
+
+
